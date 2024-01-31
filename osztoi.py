9f916fe708bff08b5234ec1558f3a827caf3548d
Kkrisi <43924835+Kkrisi@users.noreply.github.com>
2024-01-31 11:03:35 +0100

    Update osztoi.py

--- osztoi.py
+++ osztoi.py
@@ -66,5 +66,25 @@ else:
 
 
 
+#Kivalasztas tetele
+
+def Kivalasztas():
+	prim = False
+	n = 9999
+	while prim == False:
+		n += 1
+		i = 2
+		while i <= math.sqrt(n) and n % i != 0:
+			i += 1
+		prim = i > math.sqrt(n)
+	print(n)
+
+
+Kivalasztas()
+
+
+
+
+
 
 
